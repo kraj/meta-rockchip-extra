dc505367f03ccb6e501cbac48906f5542e46f796
Jacob Chen <jacob2.chen@rock-chips.com>
2017-10-14 13:57:47 +0800

    dvfs-rules: format shell script
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- recipes-rk/autostart/dvfs-rules/dvfs-rules.sh
+++ recipes-rk/autostart/dvfs-rules/dvfs-rules.sh
@@ -7,13 +7,12 @@
 COMPATIBLE=$(cat /proc/device-tree/compatible)
 
 case $1 in
-	start)
-		if [[ $COMPATIBLE =~ "rk3288" ]];
-		then
-			echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-		elif [[ $COMPATIBLE =~ "rk3036" ]]; then
-			echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-		fi
+start)
+	if [[ $COMPATIBLE =~ "rk3288" ]]; then
+		echo ondemand >/sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+	elif [[ $COMPATIBLE =~ "rk3036" ]]; then
+		echo ondemand >/sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+	fi
 	;;
 esac
 
